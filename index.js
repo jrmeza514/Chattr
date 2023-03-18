b3d7fdc26c4bc4c1639901c095686a0db218fe2e
Juan Meza <jrmeza514@gmail.com>
2023-03-18 03:00:40 -0700

    Updated socket.io

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var express = require('express');
-var io = require('socket.io');
 var app = express();
 var server = http.createServer(app);
-var socket = io( server );
+var {Server} = require('socket.io');
+var socket = new Server(server);
 
 var PORT = process.env.PORT || 8000;
 
